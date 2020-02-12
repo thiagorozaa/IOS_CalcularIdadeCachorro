47461217f9977d34920b3c71fe7f76c661b9639b
thiagorozaa <thiago.rozaa@gmail.com>
2020-02-12 17:18:51 -0300

    app para calcular idade de cachorros

--- Idade/ViewController.swift
+++ Idade/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        print("Botão pressionado")
+        
+        let idade = (Int(campoIdadeCachorro.text!)!) * 7
+        
+        legendaResultado.text = "A idade é " + String(idade) + " em anos humanos"
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("meu primeiro app")
     }
 
 
